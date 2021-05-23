b9725ff8bf2e22e61fb500f402508d548bf3d44c
Lucas <7199136+staticintlucas@users.noreply.github.com>
2021-05-23 21:48:21 +0100

    Migrate from cargo-husky to pre-commit for git hooks

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,6 @@
 // TODO add docs and don't allow these
 #![allow(missing_docs, clippy::missing_panics_doc, clippy::missing_errors_doc)]
 
-// Remove unused dependency warning
-#[cfg(test)]
-use cargo_husky as _;
-
 mod error;
 mod interfaces;
 mod layout;
