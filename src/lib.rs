5c521624b14b7e38d886a76d75fc5ed978b85ca5
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2023-03-12 21:30:19 +0000

    Reduce set of clippy allows

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
 #![warn(clippy::all, clippy::pedantic, clippy::cargo)]
-#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::cast_precision_loss, clippy::module_name_repetitions)]
 // TODO add docs and don't allow these
-#![allow(
-    missing_docs,
-    clippy::missing_panics_doc,
-    clippy::missing_errors_doc,
-    dead_code,
-    clippy::cast_precision_loss
-)]
+#![allow(missing_docs, clippy::missing_errors_doc, dead_code)]
 
 mod drawing;
 mod error;
