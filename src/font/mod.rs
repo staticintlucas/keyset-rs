812555296efb372ece618d9f23bc2583eba2ae72
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2023-03-13 00:03:29 +0000

    Parse some more font info; add tests

--- src/font/mod.rs
+++ src/font/mod.rs
@@ -5,6 +5,13 @@ use ttf_parser::Face;
 #[derive(Debug)]
 pub struct Font {
     name: String,
+    em_size: f32,
+    cap_height: f32,
+    x_height: f32,
+    ascent: f32,
+    descent: f32,
+    line_height: f32,
+    slope: f32,
 }
 
 impl Font {
@@ -18,6 +25,43 @@ impl Font {
             .find_map(|n| n.to_string())
             .unwrap_or("unknown".to_string());
 
-        Ok(Self { name })
+        let em_size = f32::from(face.units_per_em());
+        let cap_height = f32::from(face.capital_height().unwrap_or(0)); // TODO calculate default
+        let x_height = f32::from(face.x_height().unwrap_or(0)); // TODO calculate default
+        let ascent = f32::from(face.ascender());
+        let descent = f32::from(-face.descender());
+        let line_height = ascent + descent + f32::from(face.line_gap());
+        let slope = face.italic_angle().unwrap_or(0.);
+
+        Ok(Self {
+            name,
+            em_size,
+            cap_height,
+            x_height,
+            ascent,
+            descent,
+            line_height,
+            slope,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use assert_approx_eq::assert_approx_eq;
+
+    #[test]
+    fn test_from_ttf() {
+        let data = std::fs::read("open-cherry/OpenCherry-Regular.otf").unwrap();
+        let font = Font::from_ttf(&data).unwrap();
+
+        assert_eq!(font.name, "Open Cherry");
+        assert_approx_eq!(font.em_size, 1e3);
+        assert_approx_eq!(font.cap_height, 700.);
+        assert_approx_eq!(font.x_height, 500.);
+        assert_approx_eq!(font.line_height, 1200.);
+        assert_approx_eq!(font.slope, 0.);
     }
 }
