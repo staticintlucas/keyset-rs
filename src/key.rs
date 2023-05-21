c4da31ded8d81376bd91f608fbf70fa7b90bd68d
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2023-05-21 16:12:24 +0100

    Add new key module intended to replace existing one

---
+++ src/key.rs
@@ -0,0 +1,114 @@
+use crate::utils::{Color, Vec2};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Homing {
+    Scoop,
+    Bar,
+    Bump,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Type {
+    None,   // a.k.a. decal in KLE lingo
+    Normal, // Just a regular ol' key
+    Homing(Option<Homing>),
+    Space,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Shape {
+    Normal(Vec2),
+    SteppedCaps,
+    IsoVertical,
+    IsoHorizontal,
+}
+
+impl Shape {
+    #[inline]
+    pub fn size(self) -> Vec2 {
+        match self {
+            Self::Normal(s) => s,
+            Self::IsoHorizontal | Self::IsoVertical => Vec2::new(1.5, 2.0),
+            Self::SteppedCaps => Vec2::new(1.75, 1.0),
+        }
+    }
+}
+
+impl From<Vec2> for Shape {
+    fn from(value: Vec2) -> Self {
+        Self::Normal(value)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Legend {
+    pub text: String,
+    pub size: f32,
+    pub color: Color,
+}
+
+#[derive(Debug, Clone)]
+pub struct Key {
+    pub position: Vec2,
+    pub shape: Shape,
+    pub typ: Type,
+    pub color: Color,
+    pub legend: [[Option<Legend>; 3]; 3],
+}
+
+impl Key {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Default for Key {
+    fn default() -> Self {
+        Self {
+            position: Vec2::ZERO,
+            shape: Shape::Normal(Vec2::from(1.)),
+            typ: Type::Normal,
+            color: Color::default_key(),
+            legend: Default::default(), // [[None; 3]; 3] won't work since Option<Legend> : !Copy
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    use assert_matches::assert_matches;
+
+    #[test]
+    fn test_shape_size() {
+        assert_eq!(
+            Shape::Normal(Vec2::new(2.25, 1.)).size(),
+            Vec2::new(2.25, 1.)
+        );
+        assert_eq!(Shape::IsoVertical.size(), Vec2::new(1.5, 2.0));
+        assert_eq!(Shape::IsoHorizontal.size(), Vec2::new(1.5, 2.0));
+        assert_eq!(Shape::SteppedCaps.size(), Vec2::new(1.75, 1.0));
+    }
+
+    #[test]
+    fn test_shape_from() {
+        let shape = Shape::from(Vec2::new(1.75, 1.));
+        assert_matches!(shape, Shape::Normal(x) if x == Vec2::new(1.75, 1.));
+    }
+
+    #[test]
+    fn test_key_new() {
+        let key = Key::new();
+
+        assert_eq!(key.position, Vec2::new(0., 0.));
+        assert_eq!(key.shape, Shape::Normal(Vec2::new(1., 1.)));
+        assert_eq!(key.typ, Type::Normal);
+        assert_eq!(key.color, Color::default_key());
+        for row in key.legend {
+            for el in row {
+                assert!(el.is_none());
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 mod drawing;
 mod error;
 mod font;
+mod key;
 mod kle;
 mod layout;
 mod profile;
