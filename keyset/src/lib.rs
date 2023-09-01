12aea45aa2b7d1b3b7ce9a39f2a4310847c2d393
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2023-09-01 13:26:18 +0100

    Add clippy lints to every lib.rs

--- keyset/src/lib.rs
+++ keyset/src/lib.rs
@@ -1,1 +1,13 @@
+#![warn(
+    clippy::all,
+    clippy::correctness,
+    clippy::suspicious,
+    clippy::style,
+    clippy::complexity,
+    clippy::perf,
+    clippy::pedantic,
+    clippy::cargo,
+    clippy::nursery
+)]
+
 pub use keyset_core::*;
