76c25350ad3d638a78e15fdc7e9120c22c1290fc
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2024-04-25 22:24:14 +0100

    Move geom traits to their own file

--- keyset-geom/src/lib.rs
+++ keyset-geom/src/lib.rs
@@ -22,16 +22,17 @@
 mod circle;
 mod path;
 mod round_rect;
+mod traits;
 mod unit;
-
-pub use unit::{
-    Dot, Inch, Mm, ToTransform, Unit, DOT_PER_INCH, DOT_PER_MM, DOT_PER_UNIT, INCH_PER_UNIT,
-    MM_PER_UNIT,
-};
 
 pub use circle::Circle;
 pub use path::{Path, PathBuilder, PathSegment, ToPath};
 pub use round_rect::RoundRect;
+pub use traits::*;
+pub use unit::{
+    Dot, Inch, Mm, ToTransform, Unit, DOT_PER_INCH, DOT_PER_MM, DOT_PER_UNIT, INCH_PER_UNIT,
+    MM_PER_UNIT,
+};
 
 pub use euclid::approxeq::ApproxEq;
 
@@ -61,64 +62,3 @@ pub type Transform<U, V> = euclid::Transform2D<f32, U, V>;
 
 /// A 2-dimensional vector with unit `U`
 pub type Vector<U> = euclid::Vector2D<f32, U>;
-
-/// Trait to add additional constructor to `Rect`
-pub trait ExtRect<U> {
-    /// Create a new `Rect` given a center point and size
-    fn from_center_and_size(center: Point<U>, size: Size<U>) -> Self;
-}
-
-impl<U> ExtRect<U> for Rect<U> {
-    fn from_center_and_size(center: Point<U>, size: Size<U>) -> Self {
-        Self::from_origin_and_size(center - size / 2.0, size)
-    }
-}
-
-/// Trait to rotate a `Vector`
-pub trait ExtVec<T, U> {
-    /// Rotate the vector by the given angle
-    #[must_use]
-    fn rotate(self, angle: T) -> Self;
-
-    /// Negate the x component of the vector
-    #[must_use]
-    fn neg_x(self) -> Self;
-
-    /// Negate the y component of the vector
-    #[must_use]
-    fn neg_y(self) -> Self;
-}
-
-impl<U> ExtVec<Angle, U> for Vector<U> {
-    fn rotate(self, angle: Angle) -> Self {
-        let (sin, cos) = angle.sin_cos();
-        Self::new(self.x * cos - self.y * sin, self.x * sin + self.y * cos)
-    }
-
-    fn neg_x(self) -> Self {
-        let (x, y) = self.to_tuple();
-        Self::new(-x, y)
-    }
-
-    fn neg_y(self) -> Self {
-        let (x, y) = self.to_tuple();
-        Self::new(x, -y)
-    }
-}
-
-impl<U> ExtVec<euclid::Angle<f64>, U> for euclid::Vector2D<f64, U> {
-    fn rotate(self, angle: euclid::Angle<f64>) -> Self {
-        let (sin, cos) = angle.sin_cos();
-        Self::new(self.x * cos - self.y * sin, self.x * sin + self.y * cos)
-    }
-
-    fn neg_x(self) -> Self {
-        let (x, y) = self.to_tuple();
-        Self::new(-x, y)
-    }
-
-    fn neg_y(self) -> Self {
-        let (x, y) = self.to_tuple();
-        Self::new(x, -y)
-    }
-}
---
+++ keyset-geom/src/traits.rs
@@ -0,0 +1,62 @@
+use crate::{Angle, Point, Rect, Size, Vector};
+
+/// Trait to add additional constructor to `Rect`
+pub trait ExtRect<U> {
+    /// Create a new `Rect` given a center point and size
+    fn from_center_and_size(center: Point<U>, size: Size<U>) -> Self;
+}
+
+impl<U> ExtRect<U> for Rect<U> {
+    fn from_center_and_size(center: Point<U>, size: Size<U>) -> Self {
+        Self::from_origin_and_size(center - size / 2.0, size)
+    }
+}
+
+/// Trait to rotate a `Vector`
+pub trait ExtVec<T, U> {
+    /// Rotate the vector by the given angle
+    #[must_use]
+    fn rotate(self, angle: T) -> Self;
+
+    /// Negate the x component of the vector
+    #[must_use]
+    fn neg_x(self) -> Self;
+
+    /// Negate the y component of the vector
+    #[must_use]
+    fn neg_y(self) -> Self;
+}
+
+impl<U> ExtVec<Angle, U> for Vector<U> {
+    fn rotate(self, angle: Angle) -> Self {
+        let (sin, cos) = angle.sin_cos();
+        Self::new(self.x * cos - self.y * sin, self.x * sin + self.y * cos)
+    }
+
+    fn neg_x(self) -> Self {
+        let (x, y) = self.to_tuple();
+        Self::new(-x, y)
+    }
+
+    fn neg_y(self) -> Self {
+        let (x, y) = self.to_tuple();
+        Self::new(x, -y)
+    }
+}
+
+impl<U> ExtVec<euclid::Angle<f64>, U> for euclid::Vector2D<f64, U> {
+    fn rotate(self, angle: euclid::Angle<f64>) -> Self {
+        let (sin, cos) = angle.sin_cos();
+        Self::new(self.x * cos - self.y * sin, self.x * sin + self.y * cos)
+    }
+
+    fn neg_x(self) -> Self {
+        let (x, y) = self.to_tuple();
+        Self::new(-x, y)
+    }
+
+    fn neg_y(self) -> Self {
+        let (x, y) = self.to_tuple();
+        Self::new(x, -y)
+    }
+}
